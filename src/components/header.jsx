c0db82ce54712c60632b8ab798efa57f63d73219
bsherbrook <bsherbrook@gmail.com>
2023-08-07 12:37:47 -0400

    build react foundation

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+
+export default function Header() {
+
+    return (
+      <>
+        <div className="">
+            <AcUnitIcon />
+            <div  className="">
+
+            </div>
+        </div>
+      </>
+    )
+  }
