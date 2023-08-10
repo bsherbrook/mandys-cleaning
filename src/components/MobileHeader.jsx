766d9e6970871ba98db7a2d614fb939a6a41eaac
bsherbrook <bsherbrook@gmail.com>
2023-08-10 17:08:23 -0400

    style mobile drop down and add icons

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -1,9 +1,12 @@
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
-import './MobileHeader.css'
-import * as React from 'react';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import Button from "@mui/material/Button";
+import "./MobileHeader.css";
+import * as React from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import ContactSupportOutlinedIcon from "@mui/icons-material/ContactSupportOutlined";
 
 export default function MobileHeader(props) {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -14,19 +17,19 @@ export default function MobileHeader(props) {
   const handleClose = () => {
     setAnchorEl(null);
   };
-  function showContact(){
+  function showContact() {
     props.setContactSwitch(true);
     props.setAboutSwitch(false);
     props.setHomeSwitch(false);
     props.setThankSwitch(false);
   }
-  function showAbout(){
+  function showAbout() {
     props.setAboutSwitch(true);
     props.setHomeSwitch(false);
     props.setContactSwitch(false);
     props.setThankSwitch(false);
   }
-  function showHome(){
+  function showHome() {
     props.setHomeSwitch(true);
     props.setAboutSwitch(false);
     props.setContactSwitch(false);
@@ -36,45 +39,72 @@ export default function MobileHeader(props) {
     <>
       <div className="bg-pink-300 flex justify-around shadow-md shadow-pink-400">
         <div className="flex">
-          <div id="logo" className="text-white flex flex-row justify-center align-middle">
-            <div className="flex items-center justify-evenly gap-10">
-                <MenuRoundedIcon 
-                  id="basic-button"
-                  aria-controls={open ? 'basic-menu' : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={open ? 'true' : undefined}
-                  onClick={handleClick}
-                  className='hover:cursor-pointer hover:bg-purple-300 hover:bg-opacity-20'
-                  />
-                  <div>
-                    <Menu
-                      id="basic-menu"
-                      anchorEl={anchorEl}
-                      open={open}
-                      onClose={handleClose}
-                      MenuListProps={{
-                        'aria-labelled by': 'basic-button',
-                      }}
-                    >
-                      <MenuItem onClick={()=>{
-                        showHome();
-                        handleClose();
-                        }}>Home</MenuItem>
-                      <MenuItem onClick={()=>{
-                        showAbout();
-                        handleClose();
-                        }}>About Us</MenuItem>
-                      <MenuItem onClick={()=>{
-                        showContact();
-                        handleClose();
-                        }}>Contact</MenuItem>
-                    </Menu>
-                  </div>
-                <div id="titleText" className="text-white">
-                  Mandy's Care & Cleaning
-                </div>
+          <div
+            id="logo"
+            className="text-white flex flex-row justify-center align-middle"
+          >
+            <div className="flex items-center justify-evenly">
+              <Button
+                sx={{ml:0,mr:2}}
+                id="basic-button"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+                variant="outlined"
+                color="secondary"
+              >
+                <MenuRoundedIcon />
+              </Button>
+              <div>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                  }}
+                >
+                  <MenuItem
+                    onClick={() => {
+                      showHome();
+                      handleClose();
+                    }}
+                  >
+                    <HomeOutlinedIcon sx={{ marginRight: 2 }} />
+                    Home
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      showAbout();
+                      handleClose();
+                    }}
+                  >
+                    <InfoOutlinedIcon sx={{ marginRight: 2 }} />
+                    About Us
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      showContact();
+                      handleClose();
+                    }}
+                  >
+                    <ContactSupportOutlinedIcon sx={{ marginRight: 2 }} />
+                    Contact
+                  </MenuItem>
+                </Menu>
+              </div>
+              <div id="titleText" className="text-white">
+                Mandy's Care & Cleaning
+              </div>
             </div>
-            <img className='ml-3' src="./src/assets/logoHand.PNG" alt="a hand holding a heart" id="logoPic"/>
+            <img
+              className="ml-3"
+              src="./src/assets/logoHand.PNG"
+              alt="a hand holding a heart"
+              id="logoPic"
+            />
           </div>
         </div>
       </div>
